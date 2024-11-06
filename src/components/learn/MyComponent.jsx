5e8c0a05bd0bdf153542a8abe91da4fb533c468a
anhvn1903 <anhpanda19032002@gmail.com>
2024-11-06 14:47:40 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,19 @@
 const MyComponent = () => {
+    const var1 = "child";
+    // const hoidanit = undefined;
+    const hoidanit = {
+        name: "hoidanit",
+        age: 25
+    } 
+    // const hoidanit = [1,2,3];
+
     return (
-      <div>eric & holiday</div>
+        <>
+            <div>{JSON.stringify(hoidanit)} & holiday</div>
+            <div className="child" style={{borderRadius: "10px"}}>child</div>
+            <div>{console.log("Eric")}</div>
+        </>
+      
     )
   }
   export default MyComponent;
