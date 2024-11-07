7d3039e983c4aca61b5fd254ba2e3d0612c49b7c
anhvn1903 <anhpanda19032002@gmail.com>
2024-11-07 13:50:54 +0700

    video 33

---
+++ src/components/todo/ToDoData.jsx
@@ -0,0 +1,11 @@
+const ToDoData = () => {
+  return (
+    <>
+      <div className="todo-data">
+        <div>Learning react</div>
+        <div>Watching Youtube</div>
+      </div>
+    </>
+  );
+};
+export default ToDoData;
